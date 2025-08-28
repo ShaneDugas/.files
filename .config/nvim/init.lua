6b7b8c8da4ff772a9594a50433490e7ecedf14e7
Shane Dugas <Dugas.Shane@gmail.com>
2025-08-28 14:23:58 -0500

    Adding from astral

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,17 @@ require ("zorg")
 require ("plugin")
 colorscheme=mine
 print ("hello")
+
+vim.o.signcolumn = yes
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap =false
+vim.tabstop = 4
+
+
+vim.keymap.set('n','<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n','<leader>w', ':write<CR>')
+vim.keymap.set('n','<leader>q', ':quit<CR>')
+
+
